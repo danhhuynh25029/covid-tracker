9b8e0ae931bb970e663c373fe32d6a7a6684826e
Danh Huynh <danhhuynh250@gmail.com>
2021-08-18 21:49:38 +0700

    display api to table

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-var url = "https://api.covid19api.com/summary"
-fetch(url)
-.then(res => res.json())
-.then((out) => {
-  console.log(out)
-})
-.catch(err => { throw err })
+var xmlhttp = new XMLHttpRequest();
+var url = "https://api.covid19api.com/summary";
+
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var json = JSON.parse(this.responseText);
+        myFunction(json);
+    }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+var str = `<tr>
+    <th>Country</th>
+    <th>TotalConfirmed</th>
+    <th>TotalDeaths</th>
+  </tr>`;
+function myFunction(json) {
+    var length = json["Countries"]["length"];
+    console.log(length);
+    for(var i = 0 ; i < length ; i++){
+        var Country = json["Countries"][i]["Country"];
+        var Confirmed = json["Countries"][i]["TotalConfirmed"];
+        var Death = json["Countries"][i]["TotalDeaths"];
+        str = str +  `<tr><td>${Country}</td><td>${Confirmed}</td><td>${Death}</td></tr>`;
+        document.getElementById("content").innerHTML=str;
+    }
+}
